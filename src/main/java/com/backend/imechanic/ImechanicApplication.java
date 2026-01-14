0c7e90ec405d4045b7aff5315d5e1aae2f1ba392
Guillermo Palacios <nothicc04@gmail.com>
2026-01-14 15:43:29 -0500

    setup-project

---
+++ src/main/java/com/backend/imechanic/ImechanicApplication.java
@@ -0,0 +1,13 @@
+package com.backend.imechanic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImechanicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImechanicApplication.class, args);
+	}
+
+}
