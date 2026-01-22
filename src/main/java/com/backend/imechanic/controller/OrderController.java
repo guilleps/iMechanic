587bdbfbbc3000adeef675df6adb2e90f4f1e6dc
Guillermo Palacios <nothicc04@gmail.com>
2026-01-22 15:33:10 -0500

    IM-25 Cálculo de Total y Cierre

--- src/main/java/com/backend/imechanic/controller/OrderController.java
+++ src/main/java/com/backend/imechanic/controller/OrderController.java
@@ -1,12 +1,11 @@
 package com.backend.imechanic.controller;
 
 import com.backend.imechanic.controller.request.OrderRequest;
-import com.backend.imechanic.controller.response.ItemResponse;
+import com.backend.imechanic.controller.response.CloseOrderResponse;
 import com.backend.imechanic.controller.response.OrderResponse;
 import com.backend.imechanic.controller.response.TimelineResponse;
 import com.backend.imechanic.controller.response.UploadEvidenceResponse;
 import com.backend.imechanic.model.UserEntity;
-import com.backend.imechanic.service.ItemService;
 import com.backend.imechanic.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.NonNull;
@@ -51,7 +50,7 @@ public class OrderController {
                 .body(orderService.uploadEvidence(orderId, description, file, creator));
     }
 
-    @GetMapping("/{orderId}")
+    @GetMapping("/{orderId}/timeline")
     @PreAuthorize("hasAuthority('ROLE_WORKSHOP_ADMIN') or hasAuthority('ROLE_EMPLOYEE') or hasAuthority('ROLE_CUSTOMER')")
     public ResponseEntity<@NonNull TimelineResponse> getTimeline(
             @Valid @PathVariable Long orderId,
@@ -61,5 +60,13 @@ public class OrderController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(orderService.getTimeline(orderId, admin));
+    }
+
+    @GetMapping("/{orderId}/close")
+    @PreAuthorize("hasAuthority('ROLE_WORKSHOP_ADMIN')")
+    public ResponseEntity<@NonNull CloseOrderResponse> closeOrder(@PathVariable Long orderId, Authentication auth) {
+        UserEntity creator = (UserEntity) auth.getPrincipal();
+
+        return ResponseEntity.status(HttpStatus.OK).body(orderService.closeOrder(orderId, creator));
     }
 }
--- src/main/java/com/backend/imechanic/model/Evidence.java
+++ src/main/java/com/backend/imechanic/model/Evidence.java
@@ -5,7 +5,6 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 
